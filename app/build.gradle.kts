0612faae27bb7d510256e02ee0e919fd3e8da66a
contributory <bosuutap@alwaysdata.net>
2026-09-08 03:32:01 +0000

    feat: add ABI split config and rename CI build step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,15 @@ android {
         versionName = "2.3.11"
     }
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
+        }
+    }
+
     signingConfigs {
         create("release") {
             storeFile = System.getenv("CM_KEYSTORE_PATH")?.let { file(it) }
